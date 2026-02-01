f1816957d1aed6a9bf7a11509756893dd33d52f2
Diego Ramos <die.rg2005@gmail.com>
2026-02-01 21:42:51 +0100

    Proyecto Express con html y rutas Get

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rutas GET
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en puerto " + PORT);
+});
